9a93d3c3b9ffe7ff7fe2602d0523d4f80655e8aa
David Robillard <d@drobilla.net>
2018-09-16 22:04:57 +0200

    Remove pointless const qualifiers

--- src/qt5_in_gtk2.cpp
+++ src/qt5_in_gtk2.cpp
@@ -104,7 +104,7 @@ suil_qt_wrapper_realize(GtkWidget* w, gpointer data)
 {
 	SuilQtWrapper* const wrap = SUIL_QT_WRAPPER(w);
 	GtkSocket* const     s    = GTK_SOCKET(w);
-	const WId            id   = (const WId)gtk_socket_get_id(s);
+	const WId            id   = (WId)gtk_socket_get_id(s);
 
 	wrap->qembed->winId();
 	wrap->qembed->windowHandle()->setParent(QWindow::fromWinId(id));
